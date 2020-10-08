fe37d5fe730f9b6b61ff654ed8f7a3946e73efa4
adriel werlich <adrielwerlich@gmail.com>
2020-10-08 16:50:55 -0300

    adding search by repo in vuex

--- src/vuex.js
+++ src/vuex.js
@@ -9,22 +9,32 @@ export default new Vuex.Store({
   state: {
     searchQuery: '',
     user: null,
+    loading: false,
   },
 
   actions: {
-    async search( {commit, state} ) {
+    async searchUser( {commit, state} ) {
       try {
         const { data } = await axios.get(`https://api.github.com/users/${state.searchQuery}`)
         commit('SET_USER', data)
       } catch (error) {
-        
+        console.log('error in searching user', error)
       }
-    }
+    },
+    async searchRepo( {commit, state} ) {
+      try {
+        const { data } = await axios.get(`https://api.github.com/users/${state.user.id}/repos`)
+        commit('SET_USER', data)
+      } catch (error) {
+        console.log('error in searching user', error)
+      }
+    },
   },
 
   mutations: {
     'SET_SEARCH_QUERY': (state, payload) => state.searchQuery = payload,
-    'SET_USER': (state, payload) => state.user = payload
+    'SET_USER': (state, payload) => state.user = payload,
+    'SET_LOADING': (state, payload) => state.loading = payload
   },
 
 })
